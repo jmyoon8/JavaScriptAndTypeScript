580af9e01711d5aa03b8be54a8ebda46efff9329
yjj <jmyoon8@naver.com>
2021-06-04 15:01:53 +0900

    test

--- src/Data.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.data = void 0;
-exports.data = [
-    { id: 1, task: "장보기", complete: true },
-    { id: 2, task: "ts학습" }
-];
